1e80551c3d9c859f8d79a02a49079f48cf535f20
mrchuw <jgabrielmonteiro@hotmail.com>
2024-08-29 17:34:36 -0300

    adding exclusive cache for count command

--- bot/utils/caches.py
+++ bot/utils/caches.py
@@ -70,7 +70,14 @@ class SessionsCaches:
         self.GeneralCachedSession: SessionsCaches.GeneralCachedSession = self.GeneralCachedSession(bot)
         self.InfoCachedSession: SessionsCaches.InfoCachedSession = self.InfoCachedSession(bot)
         self.ToolsCachedSession: SessionsCaches.ToolsCachedSession = self.ToolsCachedSession(bot)
-        self.Miscellaneous: SessionsCaches.Miscellaneous = self.Miscellaneous(bot)
+
+        self.AliasCachedSession: SessionsCaches.AliasCachedSession = self.AliasCachedSession(bot)
+        self.CountCachedSession: SessionsCaches.CountCachedSession = self.CountCachedSession(bot)
+
+    async def close_all_sessions(self):
+        for session in vars(self).values():
+            if isinstance(session, CachedSession):
+                await session.close()
 
     class AdminCachedSession:
         def __init__(self, bot: Gorenmu):
@@ -200,7 +207,8 @@ class SessionsCaches:
                                                           allowed_methods=self.allowed_methods, include_headers=True, )
             self.session: CachedSession = CachedSession(cache=self.cache)
 
-    class Miscellaneous:
+
+    class AliasCachedSession:
         def __init__(self, bot: Gorenmu):
             self.bot = bot
             self.urls_expire_after = {"*/*": timedelta(minutes=30)}
@@ -211,6 +219,21 @@ class SessionsCaches:
                                           allowed_methods=self.allowed_methods, include_headers=True)
             else:
                 self.cache: SQLiteBackend = SQLiteBackend(cache_name=".cache/aiohttp-miscellaneous-requests.db",
+                                                          urls_expire_after=self.urls_expire_after,
+                                                          allowed_methods=self.allowed_methods, include_headers=True, )
+            self.session: CachedSession = CachedSession(cache=self.cache)
+
+    class CountCachedSession:
+        def __init__(self, bot: Gorenmu):
+            self.bot = bot
+            self.urls_expire_after = {"*/*": timedelta(minutes=30)}
+            self.allowed_methods = ("GET", "HEAD", "POST")
+            if "redis" in bot.__dict__:
+                self.cache = RedisBackend(cache_name=bot.config.CacheConfig.miscellaneous_namespace,
+                                          urls_expire_after=self.urls_expire_after,
+                                          allowed_methods=self.allowed_methods, include_headers=True)
+            else:
+                self.cache: SQLiteBackend = SQLiteBackend(cache_name=".cache/aiohttp-Count-requests.db",
                                                           urls_expire_after=self.urls_expire_after,
                                                           allowed_methods=self.allowed_methods, include_headers=True, )
             self.session: CachedSession = CachedSession(cache=self.cache)
