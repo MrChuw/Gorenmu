2f6f4cd00335c8fd4e79ec4ea225f45120da3f7e
MrChuw <jgabrielmonteiro@hotmail.com>
2025-03-19 01:27:03 -0300

    Clean Translate api

--- bot/apis/translate/__init__.py
+++ bot/apis/translate/__init__.py
@@ -1,8 +1,19 @@
 """Top-level package for Deep Translator"""
+"""
+
+Check the original package, this one is modified to use aiohttp/aiohttp-client-cache.
+https://pypi.org/project/deep-translator/
+https://github.com/nidhaloff/deep-translator
+
+https://pypi.org/project/aiohttp-client-cache/
+https://github.com/requests-cache/aiohttp-client-cache
+
+
+"""
 
 __copyright__ = "Copyright (C) 2020 Nidhal Baccouri"
 
-from .detection import batch_detection, single_detection
+
 from .google import GoogleTranslator
 
 __author__ = """Nidhal Baccouri"""
@@ -11,6 +22,4 @@ __version__ = "1.9.1"
 
 __all__ = [
     "GoogleTranslator",
-    "single_detection",
-    "batch_detection",
 ]
